b862b4e1adcfafc243a28f38897e37e9ad5b15a4
aquasprings <36392840+aquasprings@users.noreply.github.com>
2018-02-23 17:58:28 +0530

    Create Account.aspx.cs

---
+++ Account.aspx.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using AccountBO;
+using CustomerDAL;
+using CustomerBO;
+
+namespace CustomerUI
+{
+    public partial class account : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            this.UnobtrusiveValidationMode = UnobtrusiveValidationMode.None;
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            int cusid = Convert.ToInt32(TextBox1.Text);
+            int amount = Convert.ToInt32(TextBox2.Text);
+            string atype = DropDownList1.SelectedItem.Value;
+            DateTime dt = DateTime.Now;
+            asd.Text = ""+dt;
+
+            Accountbo A = new Accountbo(amount,atype,cusid);
+
+            CustomerMethods CM = new CustomerMethods();
+            try { int aid = CM.AddAccount(A);
+                aid1.Text = "" + aid;
+            }
+            catch (Exception Ex)
+            {
+                if (Ex.Message.Contains("ForeignKeyException"))
+                {
+                    Response.Write("The Customer Id already Exists");
+                }
+                
+            }
+            astatus.Text = "Active";
+            
+
+        }
+
+        protected void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+            //int check = Convert.ToInt32(TextBox1.Text);
+        }
+    }
+}
