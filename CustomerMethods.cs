daef5d3daec18f8e1027ffb45ccc95d874cf8ef6
aquasprings <36392840+aquasprings@users.noreply.github.com>
2018-02-24 16:25:43 +0530

    Update CustomerMethods.cs

--- CustomerMethods.cs
+++ CustomerMethods.cs
@@ -42,9 +42,6 @@ namespace CustomerDAL
 
 
         }
-
-
-
         public int AddAccount(Accountbo A)
         {
             try
@@ -83,6 +80,98 @@ namespace CustomerDAL
             {
                 throw E;
             }
+
+        }
+
+        public List<Customerbo> ViewCustomer()
+        {
+            SqlConnection con = new SqlConnection("server= intvmsql01; user id =PJ09TMS101_1718; database=DB09TMS101_1718;password= tcstvm");
+            con.Open();
+
+            SqlCommand cmd = new SqlCommand("sp_viewcustomer", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            List<Customerbo> listbo = new List<Customerbo>();
+            while (reader.Read())
+            {
+                Customerbo C = new Customerbo();
+                C.Id = Convert.ToInt32(reader["customerid"]);
+                C.Name = reader["cname"].ToString();
+                C.Dob = Convert.ToDateTime(reader["dob"]);
+                C.Gender = reader["gender"].ToString();
+                C.Contact = Convert.ToInt64(reader["contact"]);
+                C.City = reader["city"].ToString();
+
+                listbo.Add(C);
+
+            }
+            return listbo;
+        }
+        public List<Customerbo> ViewCustomerById(int id)
+        {
+            SqlConnection con = new SqlConnection("server= intvmsql01; user id =PJ09TMS101_1718; database=DB09TMS101_1718;password= tcstvm");
+            con.Open();
+
+            SqlCommand cmd = new SqlCommand("sp_viewcustomerbyid", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@id",id);
+            
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            List<Customerbo> listbo1 = new List<Customerbo>();
+            while (reader.Read())
+            {
+                
+                Customerbo C = new Customerbo();
+                C.Id = Convert.ToInt32(reader["customerid"]);
+                C.Name = reader["cname"].ToString();
+                C.Dob = Convert.ToDateTime(reader["dob"]);
+                C.Gender = reader["gender"].ToString();
+                C.Contact = Convert.ToInt64(reader["contact"]);
+                C.City = reader["city"].ToString();
+                
+                listbo1.Add(C);
+
+            }
+            return listbo1;
+        }        
+        public List<Accountbo> ViewAccount()
+        {
+            SqlConnection con = new SqlConnection("server= intvmsql01; user id =PJ09TMS101_1718; database=DB09TMS101_1718;password= tcstvm");
+            con.Open();
+
+            SqlCommand cmd = new SqlCommand("sp_viewaccount", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            List<Accountbo> listacc = new List<Accountbo>();
+            while (reader.Read())
+            {
+                Accountbo A1 = new Accountbo();
+                A1.Accountid = Convert.ToInt32(reader["accid"]);
+                A1.Amount = Convert.ToInt32(reader["amount"]);
+                A1.Acctype = reader["acctype"].ToString();
+                A1.Cid = Convert.ToInt32(reader["cusid"]);             
+
+                listacc.Add(A1);
+
+            }
+            return listacc;
+        }
+
+        public int  Delete(int id)
+        {
+            SqlConnection con = new SqlConnection("server=intvmsql01;user id=PJ09TMS101_1718;password=tcstvm;database=DB09TMS101_1718");
+            con.Open();
+
+            SqlCommand cmd = new SqlCommand("sp_deletecustomer_3", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.Parameters.AddWithValue("@id", id);
+
+            int rowaffected = cmd.ExecuteNonQuery();
+            return rowaffected;
 
         }
     }
