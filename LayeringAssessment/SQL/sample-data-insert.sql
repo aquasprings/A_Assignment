83b1e9dc3848502106a32698a3a807b4fdf9f91b
aquasprings <36392840+aquasprings@users.noreply.github.com>
2018-03-08 14:20:13 +0530

    Create sample-data-insert.sql

---
+++ LayeringAssessment/SQL/sample-data-insert.sql
@@ -0,0 +1,4 @@
+insert into tblItem_1349995 (itemName,price,manufacturingDate,quantityAvailable) values ('Power Bank', 5500, '01/12/16' ,10);
+insert into tblItem_1349995 (itemName,price,manufacturingDate,quantityAvailable) values ('Memory Card', 2000, '09/01/16' ,10);
+insert into tblItem_1349995 (itemName,price,manufacturingDate,quantityAvailable) values ('Wallet', 1000, '03/01/16', 5);
+insert into tblItem_1349995 (itemName,price,manufacturingDate,quantityAvailable) values ('Laptop', 35000, '01/01/17', 3);
